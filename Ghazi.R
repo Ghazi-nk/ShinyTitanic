9f4dc348ba164213ee095124ff46c174e4bbc233
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2024-01-10 16:25:28 +0100

    Integrated Ghazi Part in APP

--- Ghazi.R
+++ Ghazi.R
@@ -10,10 +10,14 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      radioButtons("class_selector", "Passagier Klasse",
-                   choices = c("1", "2", "3"),
-                   selected = "1"),
-      sliderInput("age_group_selector", "Altersgruppen",
+      checkboxInput("pclass_toggle", "Gesamt Übersicht", value = FALSE),
+      conditionalPanel(
+        condition = "input.pclass_toggle == false",
+        radioButtons("class_selector", "Passagier Klasse",
+                     choices = c("1", "2", "3"),
+                     selected = "1"),
+      ),
+      sliderInput("age_group_selector", "Sequenz der Altersgruppen festlegen:",
                   min = 10, max = 25, value = 10, step = 5)
     ),
     mainPanel(
@@ -23,36 +27,64 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  calculate_survival_probability <- function(data, class, age_group) {
-    total_passengers <- nrow(data)
-    survived_passengers <- sum(data$Survived == 1)
-    survival_probability <- (survived_passengers / total_passengers) * 100
-    return(survival_probability)
-  }
   
   output$survival_histogram <- renderPlot({
-    selected_class <- input$class_selector
     age_group <- input$age_group_selector
     
-    survival_prob <- calculate_survival_probability(titanic_data, selected_class, age_group)
-    
-    hist_data <- titanic_data %>%
-      filter(Pclass == as.numeric(selected_class),
-             !is.na(Age)) %>%
-      mutate(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group),
-                             labels = seq(0, 100, by = age_group)[-1]))
-    
-    survival_probabilities <- hist_data %>%
-      group_by(Age_Group) %>%
-      summarize(Survival_Probability = sum(Survived == 1) / n(),
-                Absolute_Count = n())
-    
-    ggplot(survival_probabilities, aes(x = as.factor(Age_Group), y = Survival_Probability * 100) +
-      geom_bar(stat = "identity", fill = "skyblue", color = "black") +
-      labs(title = paste("Überlebenswahrscheinlichkeit in Pclass", selected_class),
-           x = "Altersgruppen", y = "Überlebenswahrscheinlichkeit (%)") +
-      theme_minimal() +
-      scale_x_discrete(na.translate = FALSE)  })
+    if (input$pclass_toggle) {
+      hist_data <- titanic_data %>%
+        filter(!is.na(Age)) %>%
+        mutate(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group),
+                               labels = seq(0, 100, by = age_group)[-1]))
+      
+
+      survival_probabilities <- hist_data %>%
+        group_by(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group)), Pclass) %>%
+        summarize(Survival_Probability = sum(Survived == 1) / n())
+                  
+      
+      
+      ggplot(survival_probabilities, aes(x = as.factor(Age_Group), y = Survival_Probability, fill = as.factor(Pclass))) +
+        geom_bar(stat = "identity", position = "dodge", color = "black") +
+        labs(title = "Überlebenswahrscheinlichkeit nach Altersgruppen und Passagiere Klasse",
+             x = "Altersgruppen", y = "Absolute Anzahl der Passagiere",
+             fill = "Passagier Klasse") +  # Hier wird die Legendenbeschriftung hinzugefügt
+        scale_x_discrete(na.translate = FALSE) +
+        theme_minimal() +
+        scale_fill_manual(values = c("#FFD700", "#C0C0C0", "#CD7F32"), name = "Passagier Klasse") +  # Hier wird die Legendenüberschrift angepasst
+        theme(axis.title.y.right = element_text(color = "skyblue"),
+              axis.text.y.right = element_text(color = "skyblue"))
+      
+    } else {
+      
+      
+      
+      selected_class <- as.numeric(input$class_selector)
+      
+      hist_data <- titanic_data %>%
+        filter(Pclass == selected_class, !is.na(Age)) %>%
+        mutate(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group),
+                               labels = seq(0, 100, by = age_group)[-1]))
+      
+      survival_probabilities <- hist_data %>%
+        group_by(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group))) %>%
+        summarize(Survival_Probability = sum(Survived == 1) / n(),
+                  Absolute_Count = n())
+      
+      
+      ggplot(survival_probabilities, aes(x = as.factor(Age_Group), y = Survival_Probability * 100)) +
+        geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+        geom_text(aes(label = Absolute_Count), vjust = -0.5, color = "black") +
+        labs(title = paste("Überlebenswahrscheinlichkeit in Passagiere Klasse", selected_class),
+             x = "Altersgruppen", y = "Überlebenswahrscheinlichkeit (%)") +
+        theme_minimal() +
+        scale_x_discrete(na.translate = FALSE) +
+        scale_y_continuous(limits = c(0, 100), name = "Überlebenswahrscheinlichkeit (%)") +
+        theme(axis.title.y.right = element_text(color = "skyblue"),
+              axis.text.y.right = element_text(color = "skyblue"))
+    }
+  })
+  
 }
 
 shinyApp(ui, server)
--- app.R
+++ app.R
@@ -8,10 +8,10 @@
 #    http://shiny.rstudio.com/
 #
 
-install.packages("shiny")
-install.packages("ggplot2")
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(palmerpenguins)
 
 titanic_data <- read.csv2("titanic_data.csv", header = TRUE, sep= ",")
 # This assumes you have a CSV file named "titanic_data.csv" with the provided data.
@@ -20,6 +20,23 @@ titanic_data <- read.csv2("titanic_data.csv", header = TRUE, sep= ",")
 ui <- fluidPage(
   titlePanel("Titanic Survival Analysis"),
   
+  sidebarLayout(
+    sidebarPanel(
+      checkboxInput("pclass_toggle", "Gesamt Übersicht", value = FALSE),
+      conditionalPanel(
+        condition = "input.pclass_toggle == false",
+        radioButtons("class_selector", "Passagier Klasse",
+                     choices = c("1", "2", "3"),
+                     selected = "1"),
+      ),
+      sliderInput("age_group_selector", "Sequenz der Altersgruppen festlegen:",
+                  min = 10, max = 25, value = 10, step = 5)
+    ),
+    mainPanel(
+      plotOutput("survival_histogram")
+    )
+  ),
+
   sidebarLayout(
     sidebarPanel(
       selectInput("variable", "Choose a variable:",
@@ -105,16 +122,66 @@ server <- function(input, output) {
                      "Gender" = titanic_data_filtered$Sex,
                      "Age" = cut(as.numeric(titanic_data_filtered$Age), breaks = breaks),
                      "Fare" = cut(as.numeric(titanic_data_filtered$Fare), breaks = breaks_fare))
-    
     survival <- titanic_data_filtered$Survived
-    
     table_data <- data.frame(chosen, survival)
     colnames(table_data) <- c(input$mplot_merkmal, "Survived")
     mosaic_data <- table(table_data)
-    
     mosaicplot(mosaic_data, labs(title = "Mosaikplot"))
+    
+    #Ghazi Plot
+    output$survival_histogram <- renderPlot({
+      age_group <- input$age_group_selector
+      
+      if (input$pclass_toggle) {
+        hist_data <- titanic_data %>%
+          filter(!is.na(Age)) %>%
+          mutate(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group),
+                                 labels = seq(0, 100, by = age_group)[-1]))
+      
+        survival_probabilities <- hist_data %>%
+          group_by(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group)), Pclass) %>%
+          summarize(Survival_Probability = sum(Survived == 1) / n())
+      
+        ggplot(survival_probabilities, aes(x = as.factor(Age_Group), y = Survival_Probability, fill = as.factor(Pclass))) +
+          geom_bar(stat = "identity", position = "dodge", color = "black") +
+          labs(title = "Überlebenswahrscheinlichkeit nach Altersgruppen und Passagiere Klasse",
+               x = "Altersgruppen", y = "Absolute Anzahl der Passagiere",
+               fill = "Passagier Klasse") +  # Hier wird die Legendenbeschriftung hinzugefügt
+          scale_x_discrete(na.translate = FALSE) +
+          theme_minimal() +
+          scale_fill_manual(values = c("#FFD700", "#C0C0C0", "#CD7F32"), name = "Passagier Klasse") +  # Hier wird die Legendenüberschrift angepasst
+          theme(axis.title.y.right = element_text(color = "skyblue"),
+                axis.text.y.right = element_text(color = "skyblue"))
+              } else {
+        selected_class <- as.numeric(input$class_selector)
+        
+        hist_data <- titanic_data %>%
+          filter(Pclass == selected_class, !is.na(Age)) %>%
+          mutate(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group),
+                                 labels = seq(0, 100, by = age_group)[-1]))
+        
+        survival_probabilities <- hist_data %>%
+          group_by(Age_Group = cut(as.numeric(Age), breaks = seq(0, 100, by = age_group))) %>%
+          summarize(Survival_Probability = sum(Survived == 1) / n(),
+                    Absolute_Count = n())
+        
+        
+        ggplot(survival_probabilities, aes(x = as.factor(Age_Group), y = Survival_Probability * 100)) +
+          geom_bar(stat = "identity", fill = "skyblue", color = "black") +
+          geom_text(aes(label = Absolute_Count), vjust = -0.5, color = "black") +
+          labs(title = paste("Überlebenswahrscheinlichkeit in Passagiere Klasse", selected_class),
+               x = "Altersgruppen", y = "Überlebenswahrscheinlichkeit (%)") +
+          theme_minimal() +
+          scale_x_discrete(na.translate = FALSE) +
+          scale_y_continuous(limits = c(0, 100), name = "Überlebenswahrscheinlichkeit (%)") +
+          theme(axis.title.y.right = element_text(color = "skyblue"),
+                axis.text.y.right = element_text(color = "skyblue"))
+        
+      }
+      
+    }
+    )
   })
-View(titanic_data)
 }
 # Run the application
 shinyApp(ui = ui, server = server)
